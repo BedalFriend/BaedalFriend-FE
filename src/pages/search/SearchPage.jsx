61f46254a26275af76b75cf962e485e59fa0ad36
mingki831 <mingki831@naver.com>
2022-11-16 23:02:56 +0900

    Feat/search/검색어기초

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -8,6 +8,8 @@ import Card from '../../components/elements/card/Card';
 
 export default function SearchPage() {
 
+    //정렬 모달창 관련
+
     const [isOpen, setIsOpen] = useState(false);
 
     const openModal = () => {
@@ -18,22 +20,31 @@ export default function SearchPage() {
         setIsOpen(false);
     }
 
+    //정렬 모달 선택 관련
+
     const [select, setSelect] = useState("마감 임박 순");
 
     useEffect (() => {
         setIsOpen(false)
     }, [select])
 
+
+    //검색어
+    const [searchTerm, setSearchTerm] = useState("");
+    console.log(searchTerm);
+    
+    //enter키로 검색
+    const onSubmitSearch = (e) => {
+        if (e.key === "Enter") {
+            //엔터 눌렀을 때 동작할 코드
+        }
+    }
+    
+
     return (
         <Layout>
             <SearchST.Search>
-                <svg
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-                >
+                <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
                 <g mask='url(#mask0_329_44)'>
                     <path
                     d='M18.9 20.3L13.3 14.7C12.8 15.1 12.225 15.4167 11.575 15.65C10.925 15.8833 10.2333 16 9.5 16C7.68333 16 6.146 15.371 4.888 14.113C3.62933 12.8543 3 11.3167 3 9.5C3 7.68333 3.62933 6.14567 4.888 4.887C6.146 3.629 7.68333 3 9.5 3C11.3167 3 12.8543 3.629 14.113 4.887C15.371 6.14567 16 7.68333 16 9.5C16 10.2333 15.8833 10.925 15.65 11.575C15.4167 12.225 15.1 12.8 14.7 13.3L20.325 18.925C20.5083 19.1083 20.6 19.3333 20.6 19.6C20.6 19.8667 20.5 20.1 20.3 20.3C20.1167 20.4833 19.8833 20.575 19.6 20.575C19.3167 20.575 19.0833 20.4833 18.9 20.3ZM9.5 14C10.75 14 11.8127 13.5627 12.688 12.688C13.5627 11.8127 14 10.75 14 9.5C14 8.25 13.5627 7.18733 12.688 6.312C11.8127 5.43733 10.75 5 9.5 5C8.25 5 7.18733 5.43733 6.312 6.312C5.43733 7.18733 5 8.25 5 9.5C5 10.75 5.43733 11.8127 6.312 12.688C7.18733 13.5627 8.25 14 9.5 14Z'
@@ -43,8 +54,20 @@ export default function SearchPage() {
                 </svg>
 
                 <SearchST.SearchText
-                    type="text"
-                    placeholder='검색어를 입력해주세요.'/>
+                    type="search"
+                    placeholder='검색어를 입력해주세요.'
+                    onChange={(e) => {setSearchTerm(e.target.value);}}
+                    onKeyPress={onSubmitSearch}
+                />
+{/* 
+                {dummyData.filter((val) => {
+                        if(searchTerm == "") {
+                            return val;
+                        } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                            return val;
+                        }}
+                    ).map(data => { return <p>dummyData.title</p>})} */}
+                
             </SearchST.Search>
 
             <SearchST.RecentSection>
