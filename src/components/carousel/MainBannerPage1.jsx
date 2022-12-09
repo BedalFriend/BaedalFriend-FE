73035cf4f3d1eacd896a4138538e5d44c6043ad1
JM <jmkacc99@gmail.com>
2022-12-09 19:50:55 +0900

    Open Graph

--- src/components/carousel/MainBannerPage1.jsx
+++ src/components/carousel/MainBannerPage1.jsx
@@ -3,7 +3,6 @@ import * as CaroST from './CarouselStyle';
 import MainBanner1 from '../../imgs/carousel/MainBanner1.png';
 
 export default function MainBannerPage1() {
-
   return (
     <>
       <CaroST.BannerImg src={MainBanner1} alt='메인배너1' />
@@ -14,10 +13,11 @@ export default function MainBannerPage1() {
       </CaroST.TextBox>
       <CaroST.MiniBox
         onClick={() => {
-          window.location.replace("/login")
-        }}>
-          로그인하고 내 근처 배프 만나러 가기
+          window.location.replace('/login');
+        }}
+      >
+        로그인하고 내 근처 배프 만나러 가기
       </CaroST.MiniBox>
     </>
-  )
-};
+  );
+}
