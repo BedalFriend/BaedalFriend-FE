7bf4c335e43a7b63e0c57c288f1275b2373233cc
RohHeeJin <ranin00421@gmail.com>
2022-12-15 14:12:25 +0900

    fix: 지도 하단창 입력값 없을때 숨김

--- src/components/searchMap/SearchMapStyle.jsx
+++ src/components/searchMap/SearchMapStyle.jsx
@@ -139,7 +139,7 @@ export const CurrentBox = styled.div`
   position: absolute;
   z-index: 2;
   margin-left: 16px;
-  bottom: 255px;
+  bottom: ${(props) => (props.hideMap ? '255px' : '134px')};
 
   :hover {
     cursor: pointer;
