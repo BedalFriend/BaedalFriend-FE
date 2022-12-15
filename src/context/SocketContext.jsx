70ba46a5d3315b408ba5e751494f2e151bd85ddb
JM <jmkacc99@gmail.com>
2022-12-16 02:33:27 +0900

    Fix: Remove Socket console

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -26,12 +26,9 @@ export function SocketProvider({ children }) {
       webSocketFactory: () =>
         new SockJS(`${process.env.REACT_APP_API_URL}/ws/chat`),
       connectHeaders: {},
-      debug: (str) => {
-        console.log(str);
-      },
-      onChangeState: (e) => {
-        console.log('change!', e);
-      },
+      // debug: (str) => {
+      //   console.log(str);
+      // },
       reconnectDelay: 5000,
       heartbeatIncoming: 4000,
       heartbeatOutgoing: 4000,
